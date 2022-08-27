193c652bfb44682de915be7cc90404abc5f3b04e
chenysc <ys1208.chen@gmail.com>
2022-08-27 18:51:55 +0800

    Add test URL and test result evidence.

--- spring-boot-hello-world/src/main/java/com/mkyong/HelloController.java
+++ spring-boot-hello-world/src/main/java/com/mkyong/HelloController.java
@@ -1,5 +1,10 @@
 package com.mkyong;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +16,29 @@ public class HelloController {
         return "Hello World, Spring Boot!";
     }
 
+    
+    // My cookie test
+    @GetMapping("/myReadCookieTest")
+    public String readCookie(@CookieValue(value = "username", 
+   		                                  defaultValue = "Atta") String username) {
+    	return ("Hey! My username is " + username);
+    }
+
+    
+    @GetMapping("/change-username")
+    public String setCookie(HttpServletResponse response) {
+	    // 創建一個 cookie對象
+	    //Cookie cookie = new Cookie("username", "Jovan");
+    	Cookie cookie = new Cookie("username", "CHENYSC");
+	    //將cookie對象加入response響應
+	    response.addCookie(cookie);
+	    return ("Username is changed!");
+    }
+    
+    
+    
+    
+    
+    
+    
 }
